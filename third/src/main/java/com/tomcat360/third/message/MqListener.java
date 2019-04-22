03677f4debf2deffd14ed2c2928b1ddb28cd3b66
yezj0010 <yezj0010@163.com>
2019-04-22 17:17:19 +0800

    优化

--- third/src/main/java/com/tomcat360/third/message/MqListener.java
+++ third/src/main/java/com/tomcat360/third/message/MqListener.java
@@ -1,5 +1,6 @@
 package com.tomcat360.third.message;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -17,17 +18,18 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @EnableBinding({MqClient.class})
+@Slf4j
 public class MqListener {  //消息接受类
 
 	@StreamListener(MqClient.INPUT_A)  //监听testMessage这个消息队列, StreamClient类中必须定义相应的Input。
 //	@SendTo("testBbbQueue")  //该注解会在消息处理完成后，向responseMessage这个队列发送消息。消息内容就是该方法的返回值。
 	public void receiver(@Payload Object message){
-		System.out.println(MqClient.INPUT_A+"接收到消息："+message);
+		log.info(MqClient.INPUT_A+"接收到消息："+message);
 		//return "处理消息完成"; //当消息处理完成之后，会将该返回值发送到@SendTo指定的responseMesssage消息队列中。
 	}
 
 	@StreamListener(MqClient.INPUT_A2)
 	public void receiver2(@Payload Object message){
-		System.out.println(MqClient.INPUT_A2+"接收到消息："+message);
+		log.info(MqClient.INPUT_A2+"接收到消息："+message);
 	}
 }
